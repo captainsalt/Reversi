d859cc203c847ab5914dd0037fc1af7c8b6ecd26
David Wright <dontbothertheking@gmail.com>
2019-09-14 18:15:24 -0500

    Implement the game loop

--- Reversi/Program.fs
+++ Reversi/Program.fs
@@ -8,8 +8,22 @@ let main argv =
     let board = generateBoard 
     let players = getPlayers
 
-    //|> setTileColor White ('c', 4) 
-    //|> captureSurroundingTiles ('c', 4)
-    //|> printBoard
+    let rec gameLoop (score: Score) (player: Player) (board: GameBoard) = 
+        printBoard board
+        let coordinate = coordinatePrompt "Please enter a coordinate"
+        let tile = getTile coordinate board
+
+        match tile with 
+        | Some t -> 
+            match t.disk with 
+            | None -> 
+                let newBoard = setTileColor player.color (t.position) board
+                gameLoop score player newBoard
+            | Some _ -> 
+                gameLoop score player board
+        | None -> 
+            gameLoop score player board
+
+    gameLoop { white = 0; black = 0} { color = White } board
 
     0
