8f4f347b8cc83490438d4f51277d78c8bbfb0754
David Wright <dontbothertheking@gmail.com>
2019-09-14 20:25:30 -0500

    Implemented some game logic

--- Reversi/Program.fs
+++ Reversi/Program.fs
@@ -10,17 +10,32 @@ let main argv =
     let rec gameLoop (score: Score) (player: Player) (board: GameBoard) = 
         printBoard board
         let coordinate = coordinatePrompt "Please enter a coordinate"
-        let tile = getTile coordinate board
+        let inputTile = getTile coordinate board
 
-        match tile with 
-        | Some t -> 
-            match t.disk with 
+        match inputTile with 
+        | Some tile -> 
+            // Tile exists on the board
+            match tile.disk with 
+            // No disk is occupying the tile
             | None -> 
-                let newBoard = setTileColor player.color (t.position) board
-                gameLoop score player newBoard
+                let cappedTileList = showCapturedTiles tile (player.color) board
+
+                match cappedTileList with 
+                | [] -> 
+                    // Invalid move - No tiles were captured
+                    gameLoop score player board
+                | _ -> 
+                    // Successful capture
+                    let newBoard = 
+                        setTileColor (player.color) (tile.position) board 
+                        |> captureTiles cappedTileList (player.color)  
+
+                    gameLoop score player newBoard
             | Some _ -> 
+                // A disk is occupying the tile
                 gameLoop score player board
         | None -> 
+            // Tile does not exist
             gameLoop score player board
 
     gameLoop { white = 0; black = 0} { color = White } board
